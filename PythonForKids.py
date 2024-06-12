a72dd9db331daebd888cf54cb0b064d2374b8f5b
NishantSCOE <nishantdeshmukh996@gmail.com>
2024-06-12 17:19:50 +0530

    update

---
+++ PythonForKids.py
@@ -0,0 +1,69 @@
+# score = 1000
+# message = "I scored %s marks in exam"
+# print(message %score)
+
+# txt = "%s and %s are my best friends in %s collage"
+# frnd1 = "Rahul"
+# frnd2 = "aksh"
+# clg = "Sanjivani"
+# print(txt %(frnd1,frnd2,clg))
+
+# spaces = ' '*20
+# print('%s Date'%spaces)
+# print('%s Nishant Deshmukh'%spaces)
+# print('%s Ahmednagar'%spaces)
+# print()
+# print()
+# print('Hello my name is Nishant Deshmukh from kopargaon')
+# print('studying in Sanjivani Collage Of Engineering')
+# print()
+# print('best regards')
+# print('Nishant Deshmukh')
+# print("I Love Python "*10)
+
+# print("Im best "*1000)
+
+# mydict = {
+#     'Rahul':'fail',
+#     'Adarsh':'pass',
+#     'nishant':'pass',
+#     'soham':'pass'
+# }
+# del mydict['Adarsh']
+# mydict['nishant'] = 'fail'
+# print(mydict)
+
+# games = ['cricket','football','kho-kho','bgmi']
+# foods = ['shira','potato','samosa','brinjal']
+# favourite = games + foods
+# favourite.sort()
+# print(favourite)
+
+# ninjas = 3*25
+# samurai = 2*40
+# total = ninjas+samurai
+# print(total)
+
+# first_name = 'Nishant'
+# last_name = 'Deshmuhkh'
+# message = '"Hi there, %s %s"'
+# print(message %(first_name,last_name))
+
+# import turtle
+
+# t = turtle.Turtle()
+
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+
+# turtle.done()
+
+
+
+
+
+
